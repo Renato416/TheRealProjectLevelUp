106cd43cb99afa118a9890b1f2202414b7a568d3
Fernando-du <fernan.martineza@duocuc.cl>
2026-01-21 22:31:31 -0300

    integracion Backend 2.4.1.1 experimental

--- app/src/main/java/com/example/therealprijectlevelup/repositories/ProductRepositoryImpl.kt
+++ app/src/main/java/com/example/therealprijectlevelup/repositories/ProductRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.example.therealprijectlevelup.repositories
 
+import android.util.Log
 import com.example.therealprijectlevelup.data.api.ProductApi
 import com.example.therealprijectlevelup.models.domain.ProductDomain
 import com.example.therealprijectlevelup.utils.toDomain
@@ -10,9 +11,19 @@ class ProductRepositoryImpl @Inject constructor(
 ) : ProductRepository {
 
     override suspend fun getProducts(): List<ProductDomain> {
-        return api.getProducts()
-            ._embedded
-            .productoDTOList
-            .map { it.toDomain() }
+
+        Log.e("BACKEND_TEST", "➡️ LLAMANDO AL BACKEND")
+
+        val response = api.getProducts()
+
+        Log.e(
+            "BACKEND_TEST",
+            "✅ RESPUESTA RECIBIDA. Productos: ${response._embedded.productoDTOList.size}"
+        )
+
+        return response._embedded.productoDTOList.map {
+            Log.d("BACKEND_TEST", "🧩 Producto: ${it.name}")
+            it.toDomain()
+        }
     }
 }
